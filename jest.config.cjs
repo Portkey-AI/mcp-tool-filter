bc11b44343212cc3b93184c9a80d62209d9c3a85
Rohit Agarwal <rohit@portkey.ai>
2025-11-03 11:59:20 +0530

    chore: prepare for v1.0.0 release - fix Jest config for ES modules, update .gitignore and .npmignore

---
+++ jest.config.cjs
@@ -0,0 +1,23 @@
+module.exports = {
+  preset: 'ts-jest/presets/default-esm',
+  testEnvironment: 'node',
+  roots: ['<rootDir>/src'],
+  testMatch: ['**/__tests__/**/*.ts', '**/?(*.)+(spec|test).ts'],
+  collectCoverageFrom: [
+    'src/**/*.ts',
+    '!src/**/*.test.ts',
+    '!src/**/*.d.ts',
+  ],
+  extensionsToTreatAsEsm: ['.ts'],
+  moduleNameMapper: {
+    '^(\\.{1,2}/.*)\\.js$': '$1',
+  },
+  transform: {
+    '^.+\\.tsx?$': [
+      'ts-jest',
+      {
+        useESM: true,
+      },
+    ],
+  },
+};
